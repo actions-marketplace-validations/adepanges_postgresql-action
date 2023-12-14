e92a36a3f46873207ace71d4cd2a5d9cf8c47f38
adepanges-groupmap <ade@groupmap.com>
2023-12-14 13:54:48 +0700

    build custom postgres image

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM docker:stable
 
 COPY entrypoint.sh /entrypoint.sh
+COPY postgres.Dockerfile /postgres.Dockerfile
+
 RUN chmod +x /entrypoint.sh
+
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+doccker_build="docker buildx build -t 'custom-postgres':latest ./postgres"
+
 docker_run="docker run"
 docker_run="$docker_run -e POSTGRES_DB=$INPUT_POSTGRESQL_DB"
 docker_run="$docker_run -e POSTGRES_USER=$INPUT_POSTGRESQL_USER"
 docker_run="$docker_run -e POSTGRES_PASSWORD=$INPUT_POSTGRESQL_PASSWORD"
-docker_run="$docker_run -d -p 5432:5432 postgres:$INPUT_POSTGRESQL_VERSION"
+docker_run="$docker_run -d -p 5432:5432 'custom-postgres':latest"
 
+sh -c "$doccker_build"
 sh -c "$docker_run"
